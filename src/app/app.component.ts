9c2b28c20284f7337b0004c5ec8d9d01ce4768f4
Claudio Bertoli <elder-cb@users.noreply.github.com>
2025-11-20 11:53:28 +0000

    basic style and filtering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import {AfterViewInit, Component, OnInit, Signal, viewChild} from '@angular/core';
-import {MatTableDataSource, MatTableModule} from '@angular/material/table';
-import {MatSort, MatSortModule} from '@angular/material/sort';
-import {CommonModule} from '@angular/common';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {Galaxy} from './model/galaxy.model';
+import {AfterViewInit, Component, computed, effect, OnInit, signal, Signal, viewChild} from '@angular/core'
+import {MatTableDataSource, MatTableModule} from '@angular/material/table'
+import {MatSort, MatSortModule} from '@angular/material/sort'
+import {CommonModule} from '@angular/common'
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner'
+import {Galaxy} from './model/galaxy.model'
 import {galaxies} from '../assets/galaxies.json'
+import {FormsModule} from '@angular/forms'
+import {MatInputModule} from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatSelectModule} from '@angular/material/select'
+import {MatIconButton} from "@angular/material/button"
+import {MatIcon} from "@angular/material/icon"
 
 @Component({
   selector: 'app-root',
@@ -13,25 +19,67 @@ import {galaxies} from '../assets/galaxies.json'
     CommonModule,
     MatTableModule,
     MatSortModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    FormsModule,
+    MatIconButton,
+    MatIcon
   ],
   providers: [],
   templateUrl: 'app.component.html',
   styleUrl: 'app.component.scss'
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['id', 'name', 'type'];
-  dataSource!: MatTableDataSource<Galaxy>;
-
   matSort: Signal<MatSort> = viewChild.required(MatSort)
 
-  constructor() {}
+  displayedColumns: string[] = ['id', 'name', 'type']
+  dataSource!: MatTableDataSource<Galaxy>
+
+  galaxyTypes: string[] = [] // Holds the distinct galaxy types
+  selectedType = signal<string>('') // Model for the dropdown
+  nameSearch = signal<string>('')
+  filterValue = computed(() => {
+    return {
+      type: this.selectedType(),
+      name: this.nameSearch()
+    }
+  })
+
+  constructor() {
+    effect(() => {
+      if (this.dataSource) {
+        this.dataSource.filter = JSON.stringify({
+          type: this.selectedType(),
+          name: this.nameSearch()
+        })
+      }
+    });
+  }
 
   ngOnInit(): void {
-    this.dataSource = new MatTableDataSource(galaxies as unknown as Galaxy[]);
+    this.dataSource = new MatTableDataSource(galaxies as unknown as Galaxy[])
+    this.galaxyTypes = Array.from(new Set(galaxies.map(galaxy => galaxy.type)))
   }
 
   ngAfterViewInit() {
-    this.dataSource.sort = this.matSort();
+    this.dataSource.sort = this.matSort()
+
+    this.dataSource.filterPredicate = (data: Galaxy, filter: string) => {
+      const filters = JSON.parse(filter)
+      return (
+          !filters.name || data.name.toLowerCase().includes(filters.name.toLowerCase())) &&
+        (!filters.type || data.type === filters.type)
+    }
+
+    this.dataSource.filter = JSON.stringify({
+      type: this.selectedType(),
+      name: this.nameSearch()
+    })
+  }
+
+  clearNameSearch(): void {
+    this.nameSearch.set('')
   }
 }
